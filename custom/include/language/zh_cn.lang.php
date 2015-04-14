8c33f5fab104d2444a3fc8abca897105df594fa5
jeffcao <jeff.cao.cn@gmail.com>
2015-04-14 17:37:29 +0800

    加入下拉列表数据

--- custom/include/language/zh_cn.lang.php
+++ custom/include/language/zh_cn.lang.php
@@ -2,4 +2,55 @@
 $app_strings['LBL_GROUPTAB5_1428807079'] = '基础信息';
 
 $app_strings['LBL_GROUPTAB1_1428807143'] = '工作流程';
-?>
+$GLOBALS['app_list_strings']['fzgl_xinbie_type_list']=array (
+  'xinbie_type_man' => '男',
+  'xinbie_type_woman' => '女',
+);
+$GLOBALS['app_list_strings']['fzgls_jigou_type_list']=array (
+  'jigou_type_01' => '商业银行',
+  'jigou_type_02' => '消费金融公司',
+  'jigou_type_03' => '小贷公司',
+  'jigou_type_04' => 'P2P',
+  'jigou_type_05' => '基金公司',
+  'jigou_type_10' => '其他',
+);
+$GLOBALS['app_list_strings']['fzgls_duty_type_list']=array (
+  'duty_type_01' => '信贷顾问',
+  'duty_type_02' => '风控经理',
+  'duty_type_03' => '产品经理',
+  'duty_type_94' => '部门经理',
+);
+$GLOBALS['app_list_strings']['fzgls_yuangong_status_list']=array (
+  'yuangong_status_01' => '已离职',
+  'yuangong_status_02' => '在职',
+  'yuangong_status_03' => '离职中',
+);
+$GLOBALS['app_list_strings']['fzgl_zhengjian_type_list']=array (
+  'zhengjian_type_01' => '身份证',
+  'zhengjian_type_02' => '护照',
+  'zhengjian_type_03' => '军官证',
+);
+$GLOBALS['app_list_strings']['fzgls_daikuang_type_list']=array (
+  'daikuang_type_01' => '抵押',
+  'daikuang_type_02' => '质押',
+  'daikuang_type_03' => '保证',
+  'daikuang_type_04' => '信用',
+);
+$GLOBALS['app_list_strings']['fzgls_daikuang_apply_option_list']=array (
+  'daikuang_apply_01' => '同意',
+  'daikuang_apply_02' => '不同意',
+);
+$GLOBALS['app_list_strings']['fzgls_daikuang_apply_status_list']=array (
+  'daikuang_apply_status_01' => '制单中',
+  'daikuang_apply_status_02' => '待风控经理审核',
+  'daikuang_apply_status_03' => '待部门经理审批',
+  'daikuang_apply_status_04' => '待产品经理审批',
+  'daikuang_apply_status_05' => '待客户处理',
+  'daikuang_apply_status_06' => '完成回访',
+  'daikuang_apply_status_07' => '异常关闭',
+);
+$GLOBALS['app_list_strings']['fzgls_daikuang_apply_option2_list']=array (
+  'daikuang_apply_option_01' => '同意',
+  'daikuang_apply_option_02' => '不同意',
+  'daikuang_apply_option_03' => '异常关闭',
+);
